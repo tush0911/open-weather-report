baa34ecfe9bc4e5084bcb9913c28682e7c339926
tush0911 <tusar.asia@gmail.com>
2020-11-17 01:04:05 +0530

    Updated Conditions for Zip & Country Code and Error Handling based on API Response.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const constants = {
     BASE_URL: 'http://api.openweathermap.org/data/2.5/weather?',
 
     // Insert your API key provided from Open Weather Account
-    SECRET_KEY: '3d71fe9a4928c58a0a391f83ad5f927e'
+    SECRET_KEY: ''
 }
 
 module.exports = constants;
